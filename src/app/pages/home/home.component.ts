41c35c9e039d84d002c48a5b9ced51ff86e15117
Hugo Fernando <huginho888@outlook.com>
2022-04-10 16:38:57 -0300

    slide v1

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { SwiperOptions } from 'swiper';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent  {
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  config: SwiperOptions = {
+    pagination: {
+      el: '.swiper-pagination',
+      bulletClass: `swiper-pagination-bullet`,
+      clickable: true
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+
+    },
+    loop: true,
+    autoplay: {
+      delay: 8000,
+      disableOnInteraction: false
+    }
+  };
 
 }
