17d4bce373659b5bc79afcb7086220cf86f9191f
Hugo Fernando <huginho888@outlook.com>
2022-04-19 13:58:06 -0300

    Criando página 404

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Page404Component } from './pages/page404/page404.component';
 
 const routes: Routes = [
   {
    path: '',
    loadChildren: () => import('./principal/principal.module').then(m => m.PrincipalModule)
+  },
+  {
+    path: '**',
+    component: Page404Component
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,15 @@ import { MatIconModule } from '@angular/material/icon';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Page404Component } from './pages/page404/page404.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    FooterComponent
+    FooterComponent,
+    Page404Component,
+    Page404Component
   ],
   imports: [
     BrowserModule,
