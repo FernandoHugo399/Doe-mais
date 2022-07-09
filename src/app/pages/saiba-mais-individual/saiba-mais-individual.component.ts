630a764920f44b6cfef38c59e6e678596fc2a0b6
Hugo Fernando <huginho888@outlook.com>
2022-07-09 11:02:40 -0300

    Corrigindo erros da saiba-mais

--- src/app/pages/saiba-mais-individual/saiba-mais-individual.component.ts
+++ src/app/pages/saiba-mais-individual/saiba-mais-individual.component.ts
@@ -1,7 +1,8 @@
 import { InstitutionsService } from './../../services/institutions/institutions.service';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Institution } from 'src/app/services/institutions/institutions.model';
+import GlobalVars from 'src/app/global/global.model';
 @Component({
   selector: 'app-saiba-mais-individual',
   templateUrl: './saiba-mais-individual.component.html',
@@ -11,14 +12,18 @@ export class SaibaMaisIndividualComponent implements OnInit {
   public institution!: Institution
   public messageError!: string
   public id!: string;
-  constructor(private activatedRoute: ActivatedRoute, private institutionsService: InstitutionsService) { }
+  constructor(private activatedRoute: ActivatedRoute, private route: Router, private institutionsService: InstitutionsService) { }
 
   ngOnInit(): void {
     this.id = this.activatedRoute.snapshot.paramMap.get('id') as string
 
     this.institutionsService.getInstitutionById(this.id)
     .subscribe(
-      res => this.institution = res
+      res => this.institution = res,
+      err => {
+        GlobalVars.messageError = 'Ocorreu um erro ao carregar as informações'
+        this.route.navigate(['instituicoes'])
+      }
     )
   }
 
