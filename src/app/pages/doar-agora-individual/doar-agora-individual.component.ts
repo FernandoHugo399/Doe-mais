ea669729af37ce09638de21a88595ae4da553b1c
Hugo Fernando <huginho888@outlook.com>
2022-07-09 19:35:00 -0300

    Finalizando doar agora página

--- src/app/pages/doar-agora-individual/doar-agora-individual.component.ts
+++ src/app/pages/doar-agora-individual/doar-agora-individual.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import GlobalVars from 'src/app/global/global.model';
+import { InstitutionBankInformation } from 'src/app/services/institutions/institutions.model';
+import { InstitutionsService } from 'src/app/services/institutions/institutions.service';
 
 @Component({
   selector: 'app-doar-agora-individual',
@@ -6,10 +10,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./doar-agora-individual.component.scss']
 })
 export class DoarAgoraIndividualComponent implements OnInit {
-
-  constructor() { }
+  public institutionBankInformation!: InstitutionBankInformation
+  public messageError!: string
+  public id!: string;
+  constructor(private activatedRoute: ActivatedRoute, private route: Router, private institutionsService: InstitutionsService) { }
 
   ngOnInit(): void {
+    this.id = this.activatedRoute.snapshot.paramMap.get('id') as string
+    this.request(this.id)
+  }
+
+  public request(id: string): void {
+    this.institutionsService.GetBankInformationsByIdOffInstitution(id)
+    .subscribe(
+      res => this.institutionBankInformation = res,
+      err => {
+        GlobalVars.messageError = 'Ocorreu um erro ao carregar as informações'
+        this.route.navigate(['instituicoes'])
+      }
+    )
   }
 
 }
--- src/app/services/institutions/institutions.model.ts
+++ src/app/services/institutions/institutions.model.ts
@@ -34,7 +34,7 @@ export interface Institutions {
 }
 
 export interface InstitutionBankInformation{
-  institutionBankInformation: {
+  institutionInformation: {
     id_dados: number
     banco_pix_1: string
     pix_1: string
@@ -42,12 +42,10 @@ export interface InstitutionBankInformation{
     banco_pix_2?: string
     pix_2?: string
     qr_code_pix_2?: string
-    banco_1: string
-    agencia_banco_1: string
-    conta_banco_1: string
-    banco_2?: string
-    agencia_banco_2?: string
-    conta_banco_2?: string
+    localizacao: string
+    logo: string
+    sobre_curto: string
+    nome: string
     id_instituicao: number
   }
 }
