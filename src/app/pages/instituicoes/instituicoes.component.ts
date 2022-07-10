e21c7840a787d1cae8aaaad0a70d4395aedc3542
Hugo Fernando <huginho888@outlook.com>
2022-07-09 21:12:36 -0300

    Finalizando mensagens de erro

--- src/app/pages/instituicoes/instituicoes.component.ts
+++ src/app/pages/instituicoes/instituicoes.component.ts
@@ -2,6 +2,7 @@ import GlobalVars from 'src/app/global/global.model';
 import { InstitutionsService } from './../../services/institutions/institutions.service';
 import { Component, OnInit } from '@angular/core';
 import { Institutions } from 'src/app/services/institutions/institutions.model';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-instituicoes',
@@ -9,12 +10,11 @@ import { Institutions } from 'src/app/services/institutions/institutions.model';
   styleUrls: ['./instituicoes.component.scss']
 })
 export class InstituicoesComponent implements OnInit {
-  public messageError!: string
   public institutions!: Institutions
-  constructor(private InstitutionsService: InstitutionsService) { }
+  constructor(private InstitutionsService: InstitutionsService, private toastr: ToastrService) { }
 
   ngOnInit(): void {
-    this.messageError = GlobalVars.messageError
+    if(GlobalVars.messageError) this.showToastr(GlobalVars.messageError)
     this.getAllInstitutions()
   }
 
@@ -22,7 +22,11 @@ export class InstituicoesComponent implements OnInit {
     this.InstitutionsService.getAllInstitutions()
     .subscribe(
       res => { this.institutions = res },
-      err => this.messageError = 'Ocorreu um erro interno'
+      err => this.showToastr('Ocorreu ao carregar as instituições')
     )
+  }
+
+  showToastr(message: string): void{
+    this.toastr.error(message, 'Erro')
   }
 }
